580bc33140db9bee3acc3d11e99571e6e0150055
Joseph Flores <yosept.flores@gmail.com>
2020-04-22 12:59:44 -0500

    We added the reverseString functions and the calculator module, with 2 functionc (add, substract)

--- src/functions.js
+++ src/functions.js
@@ -5,4 +5,26 @@ const capitalize = (str) => {
   return arr.join('');
 }
 
-export default capitalize;
+const reverseString = (str) => {
+  let arr = str.split('')
+  let secondArr = [];
+  for (let i = arr.length -1; i >= 0; i--) {
+    secondArr.push(arr[i])
+  }
+
+  return secondArr.join('')
+}
+
+const calculator = (() =>{
+  const add = (...args) =>{
+    return args.reduce((total,num) => total + num)
+  }
+
+  const substract = (num, subsNum) =>{
+    return num - subsNum
+  }
+
+  return {add, substract}
+})()
+
+export {capitalize, reverseString, calculator };
--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import capitalize  from './functions';
+import {capitalize, reverseString, calculator}  from './functions';
 
 test('capitalize', () => {
   expect(typeof capitalize).toEqual('function');
@@ -6,4 +6,24 @@ test('capitalize', () => {
 
 test('capitalize', () => {
   expect(capitalize("hello")).toEqual('Hello');
+})
+
+test('reverseString', () => {
+  expect(typeof reverseString).toEqual('function');
+})
+
+test('reverseString', () => {
+  expect(reverseString("hello")).toEqual('olleh');
+})
+
+test('calculator', () => {
+  expect(typeof reverseString).toEqual('function');
+})
+
+test('calculator', () => {
+  expect(calculator.add(1,2,3)).toEqual(6);
+})
+
+test('calculator', () => {
+  expect(calculator.substract(3,2)).toEqual(1);
 })
