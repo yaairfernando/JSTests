87bb861e294aa171cac042eff43b5397153447f6
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-05-18 15:08:34 -0500

    Added readme file

---
+++ README.md
@@ -0,0 +1,49 @@
+# JS Library
+
+---
+
+In this project we made a some tests with jest
+
+---
+
+## Local installation
+
+1. Clone this repo
+```
+$ git clone git@github.com:YairFernando67/JSTests.git
+``` 
+or
+```
+$ git clone https://github.com/YairFernando67/JSTests.git
+``` 
+2. cd into the correct branch
+```
+$ cd [your desired branch]
+``` 
+2. install the dependencies
+```
+$ npm install || yarn install
+```
+3. start the tests
+```
+$ npm run test
+```
+---
+
+## Created with
+
+- JS
+
+---
+
+## Tested with
+
+- Eslint
+- JEST
+
+---
+
+## Build by 
+👤 [Yair](https://github.com/YairFernando67)
+
+👤 [Joseph](https://github.com/YoseptF)
--- src/functions.js
+++ src/functions.js
@@ -1,69 +1,65 @@
 
 const capitalize = (str) => {
-  let arr = str.split('');
-  arr[0] = arr[0].toUpperCase()
+  const arr = str.split('');
+  arr[0] = arr[0].toUpperCase();
   return arr.join('');
-}
+};
 
 const reverseString = (str) => {
-  let arr = str.split('')
-  let secondArr = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    secondArr.push(arr[i])
+  const arr = str.split('');
+  const secondArr = [];
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    secondArr.push(arr[i]);
   }
 
-  return secondArr.join('')
-}
+  return secondArr.join('');
+};
 
 const calculator = (() => {
-  const add = (...args) => {
-    return args.reduce((total, num) => total + num)
-  }
+  const add = (...args) => args.reduce((total, num) => total + num);
 
-  const multiply = (...args) => {
-    return args.reduce((total, num) => total * num)
-  }
+  const multiply = (...args) => args.reduce((total, num) => total * num);
 
-  const substract = (num, subsNum) => {
-    return num - subsNum
-  }
+  const substract = (num, subsNum) => num - subsNum;
 
-  const divide = (a, b) => {
-    return a / b
-  }
+  const divide = (a, b) => a / b;
 
-  return { add, substract, divide, multiply }
-})()
+  return {
+    add, substract, divide, multiply,
+  };
+})();
 
 const analyze = (arr) => {
-  let average = arr.reduce((prev, curr, index, arr) => {
+  const average = arr.reduce((prev, curr, index, arr) => {
     prev += curr;
     if (index === arr.length - 1) {
       return prev / arr.length;
-    } else {
-      return prev;
     }
-  }, 0)
+    return prev;
+  }, 0);
 
-  let min = Math.min.call(null, ...arr);
-  let max = Math.max.call(null, ...arr);
-  let length = arr.length;
-  return { average, min, max, length }
-}
+  const min = Math.min.call(null, ...arr);
+  const max = Math.max.call(null, ...arr);
+  const { length } = arr;
+  return {
+    average, min, max, length,
+  };
+};
 
 const caesarCipher = (message, shift) => {
-  const arr = message.split('')
+  const arr = message.split('');
   const shifted = arr.map(char => {
-    let now = char.charCodeAt(0)
-    if (now - shift < 97){
-      now = 123 - (97 - (now - shift))
-      return String.fromCharCode(now)
+    let now = char.charCodeAt(0);
+    if (now - shift < 97) {
+      now = 123 - (97 - (now - shift));
+      return String.fromCharCode(now);
     }
-    return String.fromCharCode(now - shift)
-  })
-  return shifted.join('')
-}
+    return String.fromCharCode(now - shift);
+  });
+  return shifted.join('');
+};
 
-98 - 2 | 124 - (97 - 98 - 2)
 
-export { capitalize, reverseString, calculator, analyze, caesarCipher };
+export {
+  capitalize, reverseString, calculator, analyze, caesarCipher,
+};
